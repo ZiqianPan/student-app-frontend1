6dc18a71ed258d1945dcdb594da66ee083ba8cce
ziqian pan <ziqianpan1998@gmail.com>
2023-07-19 20:23:04 -0400

    include error and loading to app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import StudentList from "./components/StudentList/StudentList";
+import Loading from "./components/Loading/Loading";
+import Error from "./components/Error/Error";
 
+// TODO: Get this value from .env
 const API_URL = "http://localhost:8888";
 
 function App() {
   const [studentData, setStudentData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
+    console.log("<App /> useEffect() fired");
     async function fetchData() {
-      const response = await fetch(`${API_URL}/students`);
-      const json = await response.json();
-      console.log("<App/> useEffect() fetcged data", json);
-      const { data } = json;
-      setStudentData(data);
+      try {
+        setLoading(true);
+        const response = await fetch(`${API_URL}/students`);
+        const json = await response.json();
+        console.log("<App /> useEffect() fetched data", json);
+        const { data, error } = json;
+        if (response.ok) {
+          setStudentData(data);
+          setLoading(false);
+        } else {
+          setError(error);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(`<App /> useEffect error : ${err.message}`);
+        setLoading(false);
+        setError(err.message);
+      }
     }
     fetchData();
   }, []);
 
-  console.log(`<App/> rendered num students = ${studentData.length}`);
-  return (
-    <div className="App">
-      <StudentList studentData={studentData} />
-    </div>
-  );
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    } else if (error) {
+      return <Error error={error} />;
+    } else {
+      return <StudentList studentData={studentData} />;
+    }
+  };
+
+  console.log(`<App /> rendered! num students = ${studentData.length}`);
+  return <div className="App">{renderContent()}</div>;
 }
 
 export default App;
